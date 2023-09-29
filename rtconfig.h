d4f956d5a5e6c592618aa2f4bfbc504db306437d
Thomas Reidemeister <thomas@labforge.ca>
2023-09-29 17:09:34 -0400

    preliminary FAT / SD support

--- rtconfig.h
+++ rtconfig.h
@@ -75,6 +75,29 @@
 
 /* DFS: device virtual file system */
 
+#define RT_USING_DFS
+#define DFS_USING_POSIX
+#define DFS_USING_WORKDIR
+#define DFS_FD_MAX 16
+#define RT_USING_DFS_V1
+#define DFS_FILESYSTEMS_MAX 4
+#define DFS_FILESYSTEM_TYPES_MAX 4
+#define RT_USING_DFS_ELMFAT
+
+/* elm-chan's FatFs, Generic FAT Filesystem Module */
+
+#define RT_DFS_ELM_CODE_PAGE 437
+#define RT_DFS_ELM_WORD_ACCESS
+#define RT_DFS_ELM_USE_LFN_3
+#define RT_DFS_ELM_USE_LFN 3
+#define RT_DFS_ELM_LFN_UNICODE_0
+#define RT_DFS_ELM_LFN_UNICODE 0
+#define RT_DFS_ELM_MAX_LFN 255
+#define RT_DFS_ELM_DRIVES 2
+#define RT_DFS_ELM_MAX_SECTOR_SIZE 512
+#define RT_DFS_ELM_REENTRANT
+#define RT_DFS_ELM_MUTEX_TIMEOUT 3000
+#define RT_USING_DFS_DEVFS
 
 /* Device Drivers */
 
@@ -87,6 +110,12 @@
 #define RT_USING_I2C_BITOPS
 #define RT_USING_PIN
 #define RT_USING_PWM
+#define RT_USING_SDIO
+#define RT_SDIO_STACK_SIZE 512
+#define RT_SDIO_THREAD_PRIORITY 15
+#define RT_MMCSD_STACK_SIZE 1024
+#define RT_MMCSD_THREAD_PREORITY 22
+#define RT_MMCSD_MAX_PARTITION 16
 #define RT_USING_TOUCH
 #define RT_TOUCH_PIN_IRQ
 
@@ -99,6 +128,7 @@
 
 /* POSIX (Portable Operating System Interface) layer */
 
+#define RT_USING_POSIX_DEVIO
 
 /* Interprocess Communication (IPC) */
 
@@ -257,6 +287,10 @@
 #define BSP_USING_I2C1
 #define BSP_I2C1_SCL_PIN 0x0202
 #define BSP_I2C1_SDA_PIN 0x0203
+#define BSP_USING_FS
+#define BSP_USING_SDCARD_FS
+#define BSP_USING_SDHI
+#define SDHI_USING_1_BIT
 #define BSP_USING_PWM
 #define BSP_USING_PWM5
 #define BSP_USING_LCD
--- src/hal_entry.c
+++ src/hal_entry.c
@@ -11,18 +11,49 @@
 #include <rtthread.h>
 #include "hal_data.h"
 #include <rtdevice.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define LED_PIN    BSP_IO_PORT_02_PIN_09 /* Onboard LED pins */
 
-void hal_entry(void)
-{
-    rt_kprintf("\nHello RT-Thread!\n");
+void hal_entry(void) {
+  rt_kprintf("\nHello RT-Thread!\n");
 
-    while (1)
-    {
-        rt_pin_write(LED_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
-        rt_pin_write(LED_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
+  /* Check filesystem */
+  rt_thread_mdelay(1000);
+  if (rt_device_find("sd0") == RT_NULL) {
+    rt_kprintf("Please insert an SD card\n");
+  }
+
+  /* Open file for reading if it exists */
+  int ret;
+  struct stat buf;
+  ret = stat("text.txt", &buf);
+  if (ret == 0) {
+    rt_kprintf("text.txt file size = %d\n", buf.st_size);
+    int fd = open("text.txt", O_RDONLY);
+    if (fd >= 0) {
+      char buf[16];
+      read(fd, buf, 13);
+      buf[13] = '\0';
+      rt_kprintf("text.txt contents: %s\n", buf);
+      close(fd);
     }
+  }
+  else {
+    int fd = open("text.txt", O_CREAT | O_WRONLY);
+    if (fd >= 0) {
+      rt_kprintf("text.txt file created\n");
+      write(fd, "Hello World!\n", 13);
+      close(fd);
+    }
+  }
+
+
+  while (1) {
+    rt_pin_write(LED_PIN, PIN_HIGH);
+    rt_thread_mdelay(500);
+    rt_pin_write(LED_PIN, PIN_LOW);
+    rt_thread_mdelay(500);
+  }
 }
